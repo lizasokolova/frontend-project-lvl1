a7e62c6676bddb4054716964bb1ab1d26a53dc3b
e.sokolova <e.sokolova@raketa.travel>
2021-03-03 13:08:40 +0300

    #add linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import gameGreeting from "../src/cli.js";
+import gameGreeting from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,9 @@ import readlineSync from 'readline-sync';
 // const name = readlineSync.question('Your answer: ');
 
 const gameGreeting = () => {
-    console.log(`May I have your name?`);
-    const userName = readlineSync.question('Your answer: ');
-    console.log(`Hello, ${userName}`);
+  console.log('May I have your name?');
+  const userName = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${userName}`);
 };
 
 export default gameGreeting;

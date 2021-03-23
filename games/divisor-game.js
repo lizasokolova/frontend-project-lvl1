2ff5abfb1b4c491a1e787db720a31b2c15eca244
e.sokolova <e.sokolova@raketa.travel>
2021-03-23 18:12:07 +0300

    #add some structure to brain-games

--- games/divisor-game.js
+++ games/divisor-game.js
@@ -1,19 +1,13 @@
 import readlineSync from 'readline-sync';
 import getRandomInt from './utils.js';
 
-// function egcd(a, b) {
-//     if (a == 0)
-//         return b;
+const calculateGCD = (a, b) => {
+    if (!b) {
+        return a;
+    }
 
-//     while (b != 0) {
-//         if (a > b)
-//             a = a - b;
-//         else
-//             b = b - a;
-//     }
-
-//     return a;
-// }
+    return calculateGCD(b, a % b);
+};
 
 const divisorGame = (userName) => {
     for (let i = 0; i < 3; i++) {
@@ -22,28 +16,17 @@ const divisorGame = (userName) => {
         console.log('Find the greatest common divisor of given numbers.');
         console.log(`Question: ${firstNumber} ${secondNumber}`);
         const checkResult = readlineSync.question('Your answer: ');
-        if ( == checkResult) { }
+        const gcd = calculateGCD(firstNumber, secondNumber);
+
+        if (calculateGCD(firstNumber, secondNumber) == checkResult) {
+            console.log('Correct!');
+        } else {
+            console.log(`${checkResult} is wrong answer ;(. Correct answer was ${gcd}. Let's try again, ${userName} !`);
+            break;
+        };
+
+        console.log(`Congratulations, ${userName}!`);
     };
 };
 
 export default divisorGame;
-
-
-// const countingGame = (userName) => {
-//     for (let i = 0; i < 3; i++) {
-//         const firstNumber = getRandomInt(0, 100);
-//         const secondNumber = getRandomInt(0, 100);
-//         console.log('What is the result of the expression?');
-//         console.log(`Question: ${firstNumber} + ${secondNumber}`);
-//         const checkResult = readlineSync.question('Your answer: ');
-//         if (firstNumber + secondNumber == checkResult) {
-//             console.log('Correct!');
-//         } else {
-//             console.log(`${checkResult} is wrong answer ;(. Correct answer was another. Let's try again, ${userName}!`);
-//             break;
-//         };
-//         console.log(`Congratulations, ${userName}!`);
-//     };
-// };
-
-// export default countingGame;

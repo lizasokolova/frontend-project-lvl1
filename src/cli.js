0b068accf9b31aabd78c67df6168e7548112ed77
e.sokolova <e.sokolova@raketa.travel>
2021-04-07 17:01:12 +0300

    refactor: fix greeting text

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ const gameGreeting = () => {
   console.log('Welcome to the Brain Games!');
   console.log('May I have your name?');
   const userName = readlineSync.question('Your answer: ');
-  console.log(`Hello, ${userName}`);
+  console.log(`Hello, ${userName}!`);
   return userName;
 };
 
